cb6c85026accd59fe00b9b7b5876ac6f3d9e822b
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-08-21 22:14:20 +0200

    Added scripts to install platform-specific packages

---
+++ install-amd-packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yay
+cat package-list-amd.txt | yay -S --noconfirm --answerclean N --answerdiff N --answeredit N -
+
---
+++ install-intel-packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yay
+cat package-list-intel.txt | yay -S --noconfirm --answerclean N --answerdiff N --answeredit N -
+
