0cc56caa5cfac61fbd67b050b96405e94eb25d3a
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-08-30 20:26:06 +0200

    Updated setupfiles

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 chmod +x install-yay.sh
 chmod +x install-packages.sh
 chmod +x install-packer-nvim.sh
+
+echo """
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+""" >> /etc/pacman.conf
+
 ./install-yay.sh
 ./install-packages.sh
 ./install-packer-nvim.sh
