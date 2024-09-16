967e4eb0744be16ed1589368ae8b95d26e149722
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-09-17 01:20:06 +0200

    Added zen browser and removed useless echo command.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@ chmod +x install-yay.sh
 chmod +x install-packages.sh
 chmod +x install-packer-nvim.sh
 
-echo """
-[multilib]
-Include = /etc/pacman.d/mirrorlist
-""" >> /etc/pacman.conf
 
 ./install-yay.sh
 ./install-packages.sh
