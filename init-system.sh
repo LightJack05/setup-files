c2f7972478d9f05eb6712b3b9c705c1139f73c30
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-12-13 17:42:30 +0100

    Replace su - with sudo -u for tty access

--- init-system.sh
+++ init-system.sh
@@ -64,15 +64,15 @@ grub-mkconfig -o /boot/grub/grub.cfg
 sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' /etc/sudoers
 
 ./user.sh
-su - LightJack05 -c '/init/setup-files/yay.sh'
-su - LightJack05 -c '/init/setup-files/packages.sh'
+sudo -u LightJack05 -c '/init/setup-files/yay.sh'
+sudo -u LightJack05 -c '/init/setup-files/packages.sh'
 ./services.sh
 
 # Set up login for github on user LightJack05
-su - LightJack05 -c 'gh auth login'
+sudo -u LightJack05 -c 'gh auth login'
 # Set up dotfiles for LightJack05
-su - LightJack05 -c 'git clone https://github.com/LightJack05/dotfiles --recursive ~/dotfiles'
-su - LightJack05 -c '~/dotfiles/setup-complete.sh'
+sudo -u LightJack05 -c 'git clone https://github.com/LightJack05/dotfiles --recursive ~/dotfiles'
+sudo -u LightJack05 -c '~/dotfiles/setup-complete.sh'
 
 # Regenerate grub config and initramfs in case user scripts made changes
 grub-mkconfig -o /boot/grub/grub.cfg
