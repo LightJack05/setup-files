6046da86285bfcc06eebc80294e2c853fc0c22e6
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-12-13 23:54:35 +0100

    Added ignoring missing services

--- services.sh
+++ services.sh
@@ -2,16 +2,16 @@
 set -euxo pipefail
 
 # Enable services based on package-list.txt
-systemctl enable NetworkManager.service
-systemctl enable avahi-daemon.service
-systemctl enable bluetooth.service
-systemctl enable cups.service
-systemctl enable illuminanced.service
-systemctl enable libvirtd.service
-systemctl enable ly@tty2.service
-systemctl enable power-profiles-daemon.service
-systemctl enable systemd-timesyncd.service
-systemctl enable ufw.service
-systemctl enable vmware-networks.service
-systemctl enable vmware-usbarbitrator.service
+systemctl enable NetworkManager.service || true
+systemctl enable avahi-daemon.service || true
+systemctl enable bluetooth.service || true
+systemctl enable cups.service || true
+systemctl enable illuminanced.service || true
+systemctl enable libvirtd.service || true
+systemctl enable ly@tty2.service || true
+systemctl enable power-profiles-daemon.service || true
+systemctl enable systemd-timesyncd.service || true
+systemctl enable ufw.service || true
+systemctl enable vmware-networks.service || true
+systemctl enable vmware-usbarbitrator.service || true
 
