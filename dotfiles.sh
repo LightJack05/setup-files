f13071f5446ca1458589605c7b25c0d60304de97
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-12-13 19:59:34 +0100

    Remove quotes around sudo commands

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # Set up login for github on user LightJack05
-sudo -u LightJack05 'gh auth login'
+sudo -u LightJack05 gh auth login
 # Set up dotfiles for LightJack05
-sudo -u LightJack05 'git clone https://github.com/LightJack05/dotfiles --recursive ~/dotfiles'
-sudo -u LightJack05 '~/dotfiles/setup-complete.sh'
+sudo -u LightJack05 git clone https://github.com/LightJack05/dotfiles --recursive ~/dotfiles
+sudo -u LightJack05 ~/dotfiles/setup-complete.sh
