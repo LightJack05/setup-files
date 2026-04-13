1669018fd05bee0353640b3087611f114f547004
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-04-13 23:07:25 +0200

    Update package list

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 echo 'Bootstrapping new system...'
-pacstrap -K /mnt base linux linux-firmware networkmanager sudo nvim vim zsh lvm2
+pacstrap -K /mnt base linux linux-firmware networkmanager sudo neovim vim zsh lvm2 sbctl efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
 
 mkdir -p /mnt/init/setup-files
